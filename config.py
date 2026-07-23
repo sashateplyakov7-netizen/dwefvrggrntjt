5e8889aee1bec0d0df8c78aad40cb5ef01876c5a
sashateplyakov7-netizen <sashateplyakov7@gmail.com>
2026-07-23 21:57:24 +0300

    Update config.py

--- config.py
+++ config.py
@@ -16,3 +16,10 @@ if DATABASE_URL:
         print(f"🔍 БОТ ПЫТАЕТСЯ ВОЙТИ ПОД ЛОГИНОМ: '{user_part}'")
     except Exception:
         pass
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+DATABASE_URL = os.getenv("DATABASE_URL")
+FREE_DAILY_LIMIT = int(os.getenv("FREE_DAILY_LIMIT", 3))
+SUB_PRICE = int(os.getenv("SUB_PRICE", 100))
+ADMIN_ID = int(os.getenv("ADMIN_ID", 0))  # <-- Добавь эту строчку
